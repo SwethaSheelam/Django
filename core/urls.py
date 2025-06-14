85d3f6b548505f0a033c88fa3007291dbed05cc6
sheelamswetha <sheelamswetha5@gmail.com>
2025-06-14 11:08:17 +0530

    urls.py updated

--- core/urls.py
+++ core/urls.py
@@ -20,7 +20,7 @@ from home.views import index,about,contact
 
 urlpatterns = [
     path('',index,name='index'),
-    path('about/',about,name='about'),
-    path('contact/',contact,name='contact'),
+    path('about-coding-brains/',about,name='about'),
+    path('contact-us/',contact,name='contact'),
     path('admin/', admin.site.urls),
 ]
